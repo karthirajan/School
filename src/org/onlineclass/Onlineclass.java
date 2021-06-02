eab0ad788d9ae128ed4d24612ea77cc825c76571
vignesh95gopi <vignesh95gopi@gmail.com>
2021-06-02 16:58:00 +0530

    added new package

---
+++ src/org/onlineclass/Onlineclass.java
@@ -0,0 +1,27 @@
+package org.onlineclass;
+
+public class Onlineclass {
+	
+		
+		public void class9() {
+			System.out.println("class 9th");
+			
+		}
+		public void class10() {
+			System.out.println("class 10th");
+			
+		}
+		public void onlineTime() {
+			String f = "class starts at 8am";
+			System.out.println(f);
+			
+		}
+		public static void main(String[] args) {
+			Onlineclass o = new Onlineclass();
+			o.class10();
+			o.class9();
+			o.onlineTime();
+		}
+	
+
+}
