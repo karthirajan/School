08228b06fa735745ce91ba08fdc031b18156bbf6
KalaiselviP <kalaics2000jp@gmail.com>
2021-06-04 00:51:01 -0500

    packages and classes modified

---
+++ src/org/String/ReverseString.java
@@ -0,0 +1,25 @@
+package org.String;
+
+import java.util.Scanner;
+
+public class ReverseString 
+{
+	public static void main(String[] arg)  
+	{  
+	ReverseString rs=new ReverseString();  
+	Scanner sc=new Scanner(System.in);  
+	System.out.print("Enter a string: ");  
+	String  str=sc.nextLine();    
+	System.out.println("Reverse of a String  is : "+rs.reversestr(str));          
+	}  
+	//reverse string method  
+	static String reversestr(String s)  
+	{  
+	String r="";  
+	for(int i=s.length();i>0;--i)         
+	{  
+	r=r+(s.charAt(i-1));   
+	}  
+	return r;  
+	}  
+}
