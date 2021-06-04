d99a13da6248c052a2c8898491a815bce3f7fbbd
KalaiselviP <kalaics2000jp@gmail.com>
2021-06-04 00:22:29 -0500

    Class and packages modified

--- src/org/fivethfloor/Dummy.java
+++ src/org/fivethfloor/Dummy.java
@@ -1,5 +1,15 @@
 package org.fivethfloor;
 
-public class Dummy {
-
-}
+public class Dummy 
+{
+	public static void main(String[] args) 
+	{ 
+			  int i,fact=1;  
+			  int number=5;
+			  for(i=1;i<=number;i++){    
+			      fact=fact*i;    
+			  }    
+			  System.out.println("Factorial of "+number+" is: "+fact);    
+	}
+	}
+	
